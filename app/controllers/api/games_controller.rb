fa269cade39944ec43d9f7c506a97a5fe9101b7c
MARIYA ELIYASEVYCH <meliyasevych@gmail.com>
2019-09-01 23:14:02 -0500

    guess

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -26,9 +26,27 @@ class Api::GamesController < ApplicationController
     @number = params[:number]
     render 'string_guess.json.jb'
   end
+
   def url_segment_params_action
     @message = params[:wildcard]
     render 'url_segment_params_view.json.jb'
   end
+
+  def segment_guess
+    target_number = 42
+    @number = params[:guess].to_i
+
+    if @number > target_number
+      @message = "You guessed too high, try a lower number."
+    elsif @number < target_number
+      @message = "You guessed too low, aim higher in life and in your guess."
+    else
+      @message = "You Win!!!"
+    end
+
+    render 'segment_guess.json.jb'
+  end
+
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     get '/whats_in_a_name'=> 'games#check_world'
     get '/string_guess' => 'games#string_guess'
     get '/url_segment_params/:wildcard/butter/:pickle_rick/philosophy' => 'games#url_segment_params_action'
+    get '/segment_guess/:guess' => 'games#segment_guess
     
   end
 end
