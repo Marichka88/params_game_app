713a8927c332666df0377d0b43139da485397a19
MARIYA ELIYASEVYCH <meliyasevych@gmail.com>
2019-09-01 22:59:06 -0500

    wildcard

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     get '/my_name' => 'games#my_name'
     get '/whats_in_a_name'=> 'games#check_world'
     get '/string_guess' => 'games#string_guess'
-    get '/url_segment_params/:wildcard' => 'games#url_segment_params_action'
+    get '/url_segment_params/:wildcard/butter/:pickle_rick/philosophy' => 'games#url_segment_params_action'
     
   end
 end
